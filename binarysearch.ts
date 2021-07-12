f86d77182d1c5ba91896c565dc2fbd4d3c980cf9
Adam Conrad <acconrad@protonmail.com>
2021-07-12 00:44:42 -0400

    binary search

---
+++ binarysearch.ts
@@ -0,0 +1,16 @@
+export function binarySearch(arr, target) {
+    if (!arr.length) return -1;
+    if (arr.length === 1) return arr[0];
+    var left = 0;
+    var right = arr.length - 1;
+    while (left !== right) {
+        var mid = left + Math.floor(right-left/2);
+        if (arr[mid] === target) return mid;
+        if (arr[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
